557f0e029993d91e1463ba9ebe37b36a66a3966d
SmokedSeaSalt <mathijsrijn@gmail.com>
2026-02-03 10:08:53 +0100

    added testcases

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -10,10 +10,17 @@ int main(int argc, char* argv[])
         std::cout << C_YELLOW << "Usage: /convert <value>\n" << C_END;
         return (1);
     }
-	if (static_cast<std::string>(argv[1]) == "test")
-	{
-
-	}
-	else
-    	ScalarConverter::convert(argv[1]);
+    if (static_cast<std::string>(argv[1]) == "test")
+    {
+        ScalarConverter::convert("123");
+        ScalarConverter::convert("e");
+        ScalarConverter::convert("42.0");
+        ScalarConverter::convert("42.0f");
+        ScalarConverter::convert("0");
+        ScalarConverter::convert("0.0");
+        ScalarConverter::convert("0.0f");
+        ScalarConverter::convert("abc");
+    }
+    else
+        ScalarConverter::convert(argv[1]);
 }
